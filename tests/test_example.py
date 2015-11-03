ce4ce9c40056276243a0c274e4cb247f01cf3ba1
Sandy Lerman <sandy@presencelearning.com>
2015-11-02 22:47:51 -0800

    First pass at an app template

---
+++ tests/test_example.py
@@ -0,0 +1,13 @@
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
+Replace this with more appropriate tests for your application.
+"""
+
+from django.test import TestCase
+
+
+class SimpleTest(TestCase):
+    def test_success(self):
+        self.assertEqual('foo', 'foo')
+
